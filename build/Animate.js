14bc89e0b68c92f1dd098ee58823b5a9d4857f09
Irfan Ghat <irfanghat@gmail.com>
2023-04-30 22:44:32 +0300

    dashboard pop up settings

--- build/Animate.js
+++ build/Animate.js
@@ -1,12 +1,30 @@
 "use strict";
+/////////////////////////////////
+// PROJECT FORM POP UP ANIMATION
+////////////////////////////////
 // ADD PROJECT BUTTON
 const addProjectBtn = document.getElementById('add-projects');
 // ADD PROJECT FORM
 const addProjectForm = document.querySelector('.add-project-form');
+// EVENT LISTENER
+addProjectBtn.addEventListener('click', displayAddProjectForm);
 // DISPLAY FORM TO ADD PROJECT
 function displayAddProjectForm(event) {
     event.preventDefault();
     addProjectForm.classList.toggle('active');
 }
-addProjectBtn.addEventListener('click', displayAddProjectForm);
+/////////////////////////////////
+// PROFILE CARD POP UP ANIMATION
+/////////////////////////////////
+// PROFILE ICON
+const profileIcon = document.querySelector('.profile');
+// PROFILE CARD
+const profileCard = document.querySelector('.profile-card');
+// EVENT LISTENER
+profileIcon.addEventListener('click', displayProfileCard);
+// DISPLAY PROFILE CARD
+function displayProfileCard(event) {
+    event.preventDefault();
+    profileCard.classList.toggle('active');
+}
 //# sourceMappingURL=Animate.js.map
--- src/Animate.ts
+++ src/Animate.ts
@@ -1,18 +1,28 @@
+/////////////////////////////////
 // PROJECT FORM POP UP ANIMATION
+////////////////////////////////
 // ADD PROJECT BUTTON
 const addProjectBtn = document.getElementById('add-projects') as HTMLElement;
-
 // ADD PROJECT FORM
 const addProjectForm = document.querySelector('.add-project-form') as HTMLFormElement;
-
+// EVENT LISTENER
+addProjectBtn.addEventListener('click', displayAddProjectForm);
 // DISPLAY FORM TO ADD PROJECT
 function displayAddProjectForm(event: Event) {
     event.preventDefault();
     addProjectForm.classList.toggle('active');
 }
-
-addProjectBtn.addEventListener('click', displayAddProjectForm);
-
-// PROFILE POP UP ANIMATION
+/////////////////////////////////
+// PROFILE CARD POP UP ANIMATION
+/////////////////////////////////
 // PROFILE ICON
-
+const profileIcon = document.querySelector('.profile') as HTMLElement;
+// PROFILE CARD
+const profileCard = document.querySelector('.profile-card') as HTMLElement;
+// EVENT LISTENER
+profileIcon.addEventListener('click', displayProfileCard);
+// DISPLAY PROFILE CARD
+function displayProfileCard(event:Event) {
+    event.preventDefault();
+    profileCard.classList.toggle('active');
+}
