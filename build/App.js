c418903ea9cc7fc366ce8b156a60aad43faa8d8c
Irfan Ghat <irfanghat@gmail.com>
2023-05-01 13:13:13 +0300

    add project functionality

--- build/App.js
+++ build/App.js
@@ -1,9 +1,188 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // DEBUGGING
 const log = console.log;
 class App {
+    ///////////////////////////
+    // READ PROJECT FORM INPUT
+    ///////////////////////////
+    static readProjectFormInput() {
+        // GET VALUES FROM INPUT FIELDS
+        const projectName = document.getElementById('project-name').value;
+        const assignedTo = document.getElementById('user-name').value;
+        const category = document.getElementById('category').value;
+        const level = document.getElementById('levels').options.selectedIndex;
+        // RETURN FORM INPUT VALUES
+        return {
+            projectName,
+            assignedTo,
+            category,
+            level
+        };
+    }
+    //////////////////////////////
+    // METHOD FOR ADDING PROJECTS
+    //////////////////////////////
     static addProject() {
-        alert('project added...');
+        return __awaiter(this, void 0, void 0, function* () {
+            // event.preventDefault();
+            // CREATE NEW PROJECT
+            const newProject = App.readProjectFormInput();
+            // CREATE POST REQUEST
+            yield fetch(`http://localhost:3000/projects`, {
+                method: 'POST',
+                body: JSON.stringify(newProject),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            log('project added...');
+        });
+    }
+    ///////////////////////////////////////
+    // METHOD TO DISPLAY ALL ADDED PROJECTS
+    ///////////////////////////////////////
+    static displayAllProjects() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // FETCH ALL PROJECTS
+            let response = yield fetch(`http://localhost:3000/projects`);
+            // CONVERT RESPONSE TO JSON
+            let projects = yield response.json();
+            //////////////////////////////
+            // SETTING PROJECT START TIME 
+            // INITIALIZE DATE OBJECT
+            //////////////////////////////
+            const date = new Date();
+            const dd = date.getDay();
+            const MM = date.getMonth();
+            const yyyy = date.getFullYear();
+            const months = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ];
+            // GET CURRENT MONTH
+            function getMonth() {
+                for (let index = 0; index < months.length; index++) {
+                    if (index == MM) {
+                        let month = months[index];
+                        return month;
+                    }
+                }
+            }
+            for (let project of projects) {
+                // SELECTOR FOR PROJECT CONTAINER | WRAPPER
+                const projectWrapper = document.querySelector('.project-wrapper');
+                let selectedLevel;
+                if (project.level == 0) {
+                    selectedLevel = null;
+                }
+                else if (project.level == 1) {
+                    selectedLevel = 'Critical';
+                }
+                else if (project.level == 2) {
+                    selectedLevel = 'Moderate';
+                }
+                else if (project.level == 3) {
+                    selectedLevel = 'Normal';
+                }
+                // PROJECT CARD CONTENT STRUCTURE
+                let projectCard = `
+        <div class="project-card">
+            <div class="action-icons">
+                <a href="#" class="update" title="Update">
+                    <ion-icon name="refresh-outline" class="update-icon"></ion-icon>
+                </a>
+
+                <a href="#" class="delete" title="Delete">
+                    <ion-icon name="trash-outline" class="delete-icon"></ion-icon>
+                </a>
+            </div>
+
+            <div class="project-category">
+                <h3>
+                    ${project.category}
+                </h3>
+            </div>
+
+            <div class="project-content">
+                <h2 class="project-name">
+                    ${project.projectName}
+                </h2>
+
+                <ul class="tasks">
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Software Upgrades
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Interfaces
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Antivirus software
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Upgrades
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="timeline">
+                <div class="start-date">
+                    <ion-icon name="calendar-outline" class="calender-icon"></ion-icon>
+
+                    <h3>
+                        Start date:
+                    </h3>
+                </div>
+
+                <p class="date">
+                    ${dd} ${getMonth()}, ${yyyy}
+                </p>
+            </div>
+        </div>
+        `;
+                projectWrapper.innerHTML += projectCard;
+            }
+        });
+    }
+    // METHOD TO GET ALL PROJECTS
+    static getAllProjects() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // FETCH ALL PROJETS
+            let response = yield fetch(`http://localhost:3000/projects`);
+            // CONVERT RESPONSE TO JSON
+            let projects = yield response.json();
+            for (let project of projects) {
+                log(project);
+            }
+        });
     }
 }
+// 
+App.displayAllProjects();
 //# sourceMappingURL=App.js.map
--- src/App.ts
+++ src/App.ts
@@ -2,11 +2,175 @@
 const log = console.log;
 
 class App {
+    ///////////////////////////
+    // READ PROJECT FORM INPUT
+    ///////////////////////////
+    static readProjectFormInput() {
+        // GET VALUES FROM INPUT FIELDS
+        const projectName = (document.getElementById('project-name')! as HTMLInputElement).value;
+        const assignedTo = (document.getElementById('user-name')! as HTMLInputElement).value;
+        const category = (document.getElementById('category')! as HTMLInputElement).value;
+        const level: number = (document.getElementById('levels') as HTMLSelectElement).options.selectedIndex;
+        // RETURN FORM INPUT VALUES
+        return {
+            projectName,
+            assignedTo,
+            category,
+            level
+        }
+    }
+    //////////////////////////////
+    // METHOD FOR ADDING PROJECTS
+    //////////////////////////////
+    static async addProject() {
+        // event.preventDefault();
+        // CREATE NEW PROJECT
+        const newProject = App.readProjectFormInput();
+        // CREATE POST REQUEST
+        await fetch(`http://localhost:3000/projects`, {
+            method: 'POST',
+            body: JSON.stringify(newProject),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        log('project added...');
+    }
+    ///////////////////////////////////////
+    // METHOD TO DISPLAY ALL ADDED PROJECTS
+    ///////////////////////////////////////
+    static async displayAllProjects() {
+        // FETCH ALL PROJECTS
+        let response = await fetch(`http://localhost:3000/projects`);
+        // CONVERT RESPONSE TO JSON
+        let projects = await response.json();
+        //////////////////////////////
+        // SETTING PROJECT START TIME 
+        // INITIALIZE DATE OBJECT
+        //////////////////////////////
+        const date = new Date();
+        const dd: number = date.getDay();
+        const MM: number = date.getMonth();
+        const yyyy: number = date.getFullYear();
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        // GET CURRENT MONTH
+        function getMonth(): any {
+            for (let index: number = 0; index < months.length; index++) {
+                if (index == MM) {
+                    let month = months[index];
+                    return month;
+                }
+            }
+        }
+        for (let project of projects) {
+            // SELECTOR FOR PROJECT CONTAINER | WRAPPER
+            const projectWrapper = document.querySelector('.project-wrapper') as HTMLDivElement;
 
-    static addProject() {
-        alert('project added...')
+            let selectedLevel:string | null;
+
+        if (project.level == 0) {
+            selectedLevel = null;
+        } else if (project.level == 1) {
+            selectedLevel = 'Critical';
+        } else if (project.level == 2) {
+            selectedLevel = 'Moderate';
+        } else if (project.level == 3) {
+            selectedLevel = 'Normal';
+        }
+
+            // PROJECT CARD CONTENT STRUCTURE
+            let projectCard = `
+        <div class="project-card">
+            <div class="action-icons">
+                <a href="#" class="update" title="Update">
+                    <ion-icon name="refresh-outline" class="update-icon"></ion-icon>
+                </a>
+
+                <a href="#" class="delete" title="Delete">
+                    <ion-icon name="trash-outline" class="delete-icon"></ion-icon>
+                </a>
+            </div>
+
+            <div class="project-category">
+                <h3>
+                    ${project.category}
+                </h3>
+            </div>
+
+            <div class="project-content">
+                <h2 class="project-name">
+                    ${project.projectName}
+                </h2>
+
+                <ul class="tasks">
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Software Upgrades
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Interfaces
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Antivirus software
+                        </a>
+                    </li>
+                    <li class="task-item">
+                        <a href="#" class="task">
+                            Upgrades
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="timeline">
+                <div class="start-date">
+                    <ion-icon name="calendar-outline" class="calender-icon"></ion-icon>
+
+                    <h3>
+                        Start date:
+                    </h3>
+                </div>
+
+                <p class="date">
+                    ${dd} ${getMonth()}, ${yyyy}
+                </p>
+            </div>
+        </div>
+        `;
+
+            projectWrapper.innerHTML += projectCard;
+
+        }
+    }
+
+    // METHOD TO GET ALL PROJECTS
+    static async getAllProjects() {
+        // FETCH ALL PROJETS
+        let response = await fetch(`http://localhost:3000/projects`);
+        // CONVERT RESPONSE TO JSON
+        let projects = await response.json();
+        for (let project of projects) {
+            log(project);
+        }
     }
 }
 
-
-
+// 
+App.displayAllProjects();
